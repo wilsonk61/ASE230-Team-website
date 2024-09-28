07aff5f4b893b9a676d98325e2e122f6058b1c33
wilsonk61 <wilsonk61@mymail.nku.edu>
2024-09-28 16:49:46 -0400

    Update index.php
    indentation fix

--- index.php
+++ index.php
@@ -3,21 +3,21 @@ require_once('functions.php');
 $team = [
     'kyle' => [
         'name' => 'Kyle',
-		'dob' => '2004-05-24',
+        'dob' => '2004-05-24',
         'role' => 'Security Analyst',
         'page' => 'detail.php?id=kyle',
         'image' => 'assets/images/kyle.png'
     ],
     'Jackson' => [
         'name' => 'Jackson',
-		'dob' => '2003-06-21',
+        'dob' => '2003-06-21',
         'role' => 'System Administrator',
         'page' => 'detail.php?id=Jackson',
         'image' => 'assets/images/Jackson.jpg'
     ],
-	'John' => [
+    'John' => [
         'name' => 'John AI Doe',
-		'dob' => '2000-06-22',
+        'dob' => '2000-06-22',
         'role' => 'Language Interpreter',
         'page' => 'detail.php?id=JaneDoe3',
         'image' => 'assets/images/chuck.jpg'
@@ -44,7 +44,7 @@ $team = [
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
     
     <!-- FontAwesome JS-->
-	<script defer src="assets/fontawesome/js/all.min.js"></script>
+    <script defer src="assets/fontawesome/js/all.min.js"></script>
        
     <!-- Theme CSS -->  
     <link id="theme-style" rel="stylesheet" href="assets/css/pillar-1.css">
@@ -54,15 +54,15 @@ $team = [
 
 <body>
     <article class="resume-wrapper text-center position-relative">
-	    <div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
-			<h1 class="py-4 text-center">OUR AMAZING TEAM</h1>
-			<?php foreach ($team as $index => $member) displayMemberCard($member, $index); ?>
-	    </div>
+        <div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
+            <h1 class="py-4 text-center">OUR AMAZING TEAM</h1>
+            <?php foreach ($team as $index => $member) displayMemberCard($member, $index); ?>
+        </div>
     </article>
 
     
     <footer class="footer text-center pt-2 pb-5">
-	    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
+        <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
         <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> by Kyle Wilson</small>
     </footer>
 
